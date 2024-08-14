7f1f7545c22d2648914d77ef6e5a2e73c71aee57
Felipe Parrondo Bastos <feliparrondobastos@gmail.com>
2024-08-13 21:09:41 -0300

    used lombok in UserDto

--- api/src/main/java/tpo/uade/api/dto/UserDto.java
+++ api/src/main/java/tpo/uade/api/dto/UserDto.java
@@ -2,13 +2,19 @@ package tpo.uade.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto implements Serializable {
 
-    @NotBlank(message = "username must not be null nor empty") //TODO -> determine error messages in util.ValidationMessages
+    @NotBlank(message = "username must not be null nor empty")
     private String username;
 
     @NotBlank(message = "email must not be null nor empty")
@@ -26,78 +32,6 @@ public class UserDto implements Serializable {
     @NotBlank(message = "surname must not be null nor empty")
     private String surname;
 
-    public UserDto(String username, String email, String password, LocalDate birthday, String name, String surname) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.birthday = birthday;
-        this.name = name;
-        this.surname = surname;
-    }
-
-    public UserDto() {
-        //No-args Constructor
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    @Override
-    public String toString() {
-        return "UserDto{" +
-                "username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", birthday=" + birthday +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                '}';
-    }
 }
 
 
