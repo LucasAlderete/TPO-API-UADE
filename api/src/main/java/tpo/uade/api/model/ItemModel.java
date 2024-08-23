e7faba29a61246f86cb579ed433d895a044443d1
Lucas <lucaspistolesi07@gmail.com>
2024-08-23 15:42:09 -0300

    added ItemModel

---
+++ api/src/main/java/tpo/uade/api/model/ItemModel.java
@@ -0,0 +1,29 @@
+package tpo.uade.api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "items")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class ItemModel {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @Column(name = "cart_id", nullable = false)
+    private CartModel cart;
+
+    @ManyToOne
+    @Column(name = "product_id", nullable = false)
+    private ProductModel product;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+}
