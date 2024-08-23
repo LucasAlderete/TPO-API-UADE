46914c4502b28d4ac4cc6a29d36362c4399cf943
Lucas <lucaspistolesi07@gmail.com>
2024-08-23 15:41:45 -0300

    added CartModel

---
+++ api/src/main/java/tpo/uade/api/model/CartModel.java
@@ -0,0 +1,30 @@
+package tpo.uade.api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carts")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class CartModel {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @Column(name = "user_id", nullable = false)
+    private UserModel user;
+
+    @OneToMany
+    private List<ItemModel> items;
+
+    @Column(name = "total")
+    private Double total;
+}
