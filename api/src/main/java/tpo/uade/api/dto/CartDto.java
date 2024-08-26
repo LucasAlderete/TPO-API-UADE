5c48128ca872574c7516ea24aac5f89c4627a9fd
Lucas <lucaspistolesi07@gmail.com>
2024-08-26 17:11:51 -0300

    added CartDto

---
+++ api/src/main/java/tpo/uade/api/dto/CartDto.java
@@ -0,0 +1,20 @@
+package tpo.uade.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartDto implements Serializable {
+    @NotBlank(message = "items must not be null nor empty")
+    private List<ItemDto> items;
+
+    @NotBlank(message = "total must not be null nor empty")
+    private Double total;
+}
