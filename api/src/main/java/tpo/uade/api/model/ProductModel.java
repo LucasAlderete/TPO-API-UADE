d5ce0ce2fb3c437ad729b227d67b2a63cd9b9461
freefrozztea <luisfalmi@gmail.com>
2024-08-26 03:36:24 -0300

    add new content for product model

--- api/src/main/java/tpo/uade/api/model/ProductModel.java
+++ api/src/main/java/tpo/uade/api/model/ProductModel.java
@@ -6,11 +6,16 @@ public class ProductModel {
     private int productId;
     private String name;
     private double price;
+    private String category;
+    private int stock;
+    private String description;
 
-    public ProductModel (int productId, String name, double price) {
+    public ProductModel (int productId, String name, double price, String category, int stock, String description) {
         this.productId = productId;
         this.name = name;
         this.price = price;
+        this.category = category;
+        this.stock = stock;
+        this.description = description;
     }
-
 }
