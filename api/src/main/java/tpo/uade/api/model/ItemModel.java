753c83703ee754f430e46f4d1382ebaa934570b8
Lucas <lucaspistolesi07@gmail.com>
2024-08-26 17:09:09 -0300

    added price to ItemModel

--- api/src/main/java/tpo/uade/api/model/ItemModel.java
+++ api/src/main/java/tpo/uade/api/model/ItemModel.java
@@ -26,4 +26,7 @@ public class ItemModel {
 
     @Column(name = "quantity", nullable = false)
     private Integer quantity;
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
 }
