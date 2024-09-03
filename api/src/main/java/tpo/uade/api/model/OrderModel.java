2353371a4ba230c50b152530f5d45eaa9ad30282
Lucas <lucaspistolesi07@gmail.com>
2024-09-03 20:36:11 -0300

    added OrderModel

---
+++ api/src/main/java/tpo/uade/api/model/OrderModel.java
@@ -0,0 +1,33 @@
+package tpo.uade.api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class OrderModel {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserModel user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItemModel> items;
+
+    @Column(name = "total")
+    private Double total;
+
+    @Column(name = "date")
+    private LocalDateTime date;
+}
