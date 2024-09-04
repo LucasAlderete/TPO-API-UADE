ba0861f7349376c09e44b36011eac83d214f14cb
Lucas <lucaspistolesi07@gmail.com>
2024-09-04 02:59:55 -0300

    added Order methods to mappers

--- api/src/main/java/tpo/uade/api/mapper/CartMapper.java
+++ api/src/main/java/tpo/uade/api/mapper/CartMapper.java
@@ -1,17 +1,20 @@
 package tpo.uade.api.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 import tpo.uade.api.dto.CartDto;
 import tpo.uade.api.model.CartModel;
+import tpo.uade.api.model.OrderModel;
 
 @Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL,
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         unmappedSourcePolicy = ReportingPolicy.IGNORE,
-        componentModel = "spring",
+        componentModel = MappingConstants.ComponentModel.SPRING,
         uses = {ItemMapper.class})
 public interface CartMapper {
     CartDto toDto(CartModel cartModel);
     CartModel toEntity(CartDto cartDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "date", expression = "java(java.time.LocalDateTime.now())")
+    OrderModel mapCartToOrder(CartModel cartModel);
 }
--- api/src/main/java/tpo/uade/api/mapper/ItemMapper.java
+++ api/src/main/java/tpo/uade/api/mapper/ItemMapper.java
@@ -3,6 +3,7 @@ package tpo.uade.api.mapper;
 import org.mapstruct.*;
 import tpo.uade.api.dto.ItemDto;
 import tpo.uade.api.model.ItemModel;
+import tpo.uade.api.model.OrderItemModel;
 
 @Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL,
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
@@ -14,4 +15,8 @@ public interface ItemMapper {
 
     @Mapping(source = "product", target = "product.name")
     ItemModel toEntity(ItemDto itemDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "order", ignore = true)
+    OrderItemModel mapItemToOrderItem(ItemModel itemModel);
 }
