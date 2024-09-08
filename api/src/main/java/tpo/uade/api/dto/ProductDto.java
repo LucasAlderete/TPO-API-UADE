efdc9f549efcdf8f0dd9d78ae7fc67e6cc60065f
Lucas Alderete <lucasm.alderete@gmail.com>
2024-09-08 16:48:49 -0300

    update productDto

--- api/src/main/java/tpo/uade/api/dto/ProductDto.java
+++ api/src/main/java/tpo/uade/api/dto/ProductDto.java
@@ -2,12 +2,21 @@ package tpo.uade.api.dto;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDto implements Serializable {
     private int productId;
     private String name;
     private BigDecimal price;
+    private String categoryName;
+    private String urlImage;
+    //Si usamos mas de una imagen dejamos el list
+    //private List<String> urlImages;
 }
