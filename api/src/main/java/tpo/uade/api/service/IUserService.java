68cb0e1f750d0eae838e808e63bbbb5230855f2f
Felipe Parrondo Bastos <feliparrondobastos@gmail.com>
2024-09-24 20:38:35 -0300

    user changes

--- api/src/main/java/tpo/uade/api/service/IUserService.java
+++ api/src/main/java/tpo/uade/api/service/IUserService.java
@@ -3,6 +3,6 @@ package tpo.uade.api.service;
 import tpo.uade.api.dto.UserDto;
 
 public interface IUserService {
-    UserDto getUserByUsername (String username);
+    UserDto getUserByUsername (String token);
     void createUser(UserDto userDto);
 }
