4c4ad0e39c6a21c8f86674453024070ca8606a22
Franco <fr.cabanillas7@gmail.com>
2024-09-24 18:11:30 -0300

    Arreglo deteccion del LocalDate(yyyy-mm-dd) de la BD

--- api/src/main/java/tpo/uade/api/config/JacksonConfig.java
+++ api/src/main/java/tpo/uade/api/config/JacksonConfig.java
@@ -2,6 +2,7 @@ package tpo.uade.api.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,8 @@ public class JacksonConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         // Configura la estrategia de nombres para que convierta camelCase a snake_case
         objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        // Añadir soporte para fechas y horas de Java 8 (LocalDate, LocalDateTime, etc.)
+        objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
     }
 }
