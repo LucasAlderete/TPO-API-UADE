e70f7fea82828d0bb331b35bba6a99b3f5bb5fcd
Franco <fr.cabanillas7@gmail.com>
2024-11-28 20:49:36 -0300

    Agrego descripcion a las funciones

--- api/src/main/java/tpo/uade/api/service/implementation/MyProfileService.java
+++ api/src/main/java/tpo/uade/api/service/implementation/MyProfileService.java
@@ -14,6 +14,7 @@ import tpo.uade.api.repository.UserRepository;
 import tpo.uade.api.service.IMyProfileService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 @Service
@@ -25,32 +26,52 @@ public class MyProfileService implements IMyProfileService {
     private final UserMyProfileMapper userMapper;
     private final OrderMapper orderMapper;
 
+    /**
+     * Obtiene usuario con sus ordenes.
+     *
+     * @return usuario con ordenes en formato DTO.
+     */
     @Override
     public UserMyProfileDto getUserWithOrders() {
         UserModel user = userService.getUserModelByUsername();
         List<OrderDto> orderList = getUserOrders(user.getUserId());
-        UserMyProfileDto userMyProfile = userMapper.toDto(user);
-        userMyProfile.setOrdersDto(orderList);
-        return userMyProfile;
+        UserMyProfileDto userDto = userMapper.toDto(user);
+        userDto.setOrdersDto(orderList);
+        return userDto;
     }
 
+    /**
+     * Obtiene las ordenes de un usuario.
+     *
+     * @param id  id del usuario.
+     * @return lista de ordenes tipo DTO o lista vacia si el usuario no tiene ordenes aun.
+     */
     @Override
     public List<OrderDto> getUserOrders(Long id) {
-        return orderRepository.findByUser_UserId(id).stream()
+        return orderRepository
+                .findByUser_UserId(id)
+                .stream()
                 .map(orderMapper::toDto)
                 .toList();
     }
 
+    /**
+     * Actualiza un usuario.
+     *
+     * @param userToUpdate usuario DTO ingresado.
+     * @return usuario DTO actualizado.
+     * @throws NoSuchElementException si no encuentra al usuario.
+     */
     @Override
-    public UserMyProfileDto setUser(UserMyProfileDto userToUpdate) {
-        UserModel currentUser = userService.getUserModelByUsername();
-        currentUser.setUsername(userToUpdate.getUsername());
-        currentUser.setBirthday(userToUpdate.getBirthday());
-        currentUser.setEmail(userToUpdate.getEmail());
-        currentUser.setName(userToUpdate.getName());
-        currentUser.setSurname(userToUpdate.getSurname());
-        userRepository.save(currentUser);
-        return userMapper.toDto(currentUser);
+    public UserMyProfileDto setUser(UserMyProfileDto userToUpdate) throws NoSuchElementException {
+        UserModel user = userService.getUserModelByUsername();
+        user.setUsername(userToUpdate.getUsername());
+        user.setBirthday(userToUpdate.getBirthday());
+        user.setEmail(userToUpdate.getEmail());
+        user.setName(userToUpdate.getName());
+        user.setSurname(userToUpdate.getSurname());
+        userRepository.save(user);
+        return userMapper.toDto(user);
     }
 
 }
