31e131e824b05c91a6fe2bfee09f5c78329dfaf1
Lucas Alderete <lucasm.alderete@gmail.com>
2024-11-30 21:50:02 -0300

    agregar, eliminar y listar favoritos funcionando con auth

--- api/src/main/java/tpo/uade/api/controller/FavoriteController.java
+++ api/src/main/java/tpo/uade/api/controller/FavoriteController.java
@@ -1,42 +1,41 @@
 package tpo.uade.api.controller;
-
-import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tpo.uade.api.dto.ProductDto;
 import tpo.uade.api.service.IFavoriteService;
 
-@Api(value = "Favs Operations")
+import java.util.List;
+
 @RestController
 @RequestMapping("/favorite")
-@Validated
+@RequiredArgsConstructor
 public class FavoriteController {
-//    private IFavoriteService favoriteService;
-//
-//    FavoriteController(IFavoriteService favoriteService) {
-//        this.favoriteService = favoriteService;
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<?> add(@RequestBody String productId) {
-//        long userId = 0L;
-//        favoriteService.add(Integer.parseInt(productId), userId);
-//        return ResponseEntity.ok().build();
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<?> remove(@PathVariable String id) {
-//        try {
-//            long userId = 0L;
-//            favoriteService.remove(Integer.parseInt(id), userId);
-//            return ResponseEntity.ok().build();
-//        } catch (Exception e){
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
+
+    private final IFavoriteService favoriteService;
+
+    @PostMapping("/{productId}")
+    public ResponseEntity<Void> add(@PathVariable Long productId) {
+        boolean success = favoriteService.add(productId);
+        if (success) {
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> remove(@PathVariable Long productId) {
+        boolean success = favoriteService.remove(productId);
+        if (success) {
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> get() {
+        return ResponseEntity.ok(favoriteService.getByUserLogged());
+    }
 }
--- api/src/main/java/tpo/uade/api/service/IFavoriteService.java
+++ api/src/main/java/tpo/uade/api/service/IFavoriteService.java
@@ -1,6 +1,11 @@
 package tpo.uade.api.service;
 
+import tpo.uade.api.dto.ProductDto;
+
+import java.util.List;
+
 public interface IFavoriteService {
-    boolean add(int productId, long user_id);
-    boolean remove(int productId, long user_id);
+    boolean add(Long productId);
+    boolean remove(Long productId);
+    List<ProductDto> getByUserLogged();
 }
